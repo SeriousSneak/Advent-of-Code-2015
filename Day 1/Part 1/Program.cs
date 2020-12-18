e077c4dc8cbcfb639078dc60a5d7d2fc2c225375
Andrew Stobart <astobart@microsoft.com>
2020-12-18 14:32:00 -0600

    Day 1 complete

---
+++ Day 1/Part 1/Program.cs
@@ -0,0 +1,53 @@
+﻿/* Advent of Code 2015
+ * 
+ * Programmer: Andrew Stobart
+ * Date: December 18,2020 
+ * 
+ * Day 1 Part 1
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var lines = File.ReadLines(@"C:\Users\astobart\OneDrive\Work\Code\Advent of Code\2015\Day 1\Part 1\input.txt");
+
+            string input = "";
+
+            //this will only be run once because there's only one line in the file
+            foreach (var line in lines)
+            {
+                input = line.ToString();
+            }
+            int count = 0;
+            foreach (char character in input)
+            {
+                if (character == '(')
+                {
+                    count++;
+                }
+                if (character == ')')
+                {
+                    count--;
+                }
+            }
+
+            Console.WriteLine("Santa will end up on floor " + count + ".");
+
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day 1/Part 2/Program.cs
@@ -0,0 +1,63 @@
+﻿/* Advent of Code 2015
+ * 
+ * Programmer: Andrew Stobart
+ * Date: December 18,2020 
+ * 
+ * Day 1 Part 2
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadLines(@"C:\Users\astobart\OneDrive\Work\Code\Advent of Code\2015\Day 1\Part 1\input.txt");
+
+            string input = "";
+
+            //this will only be run once because there's only one line in the file
+            foreach (var line in lines)
+            {
+                input = line.ToString();
+            }
+
+            int floor = 0;
+            int instructionCount = 1;
+            foreach (char character in input)
+            {
+                if (character == '(')
+                {
+                    floor++;
+                }
+                if (character == ')')
+                {
+                    floor--;
+                }
+
+                if (floor == -1)
+                {
+                    //break out of the Foreach loop
+                    break;
+                }
+
+
+                instructionCount++;
+            }
+
+            Console.WriteLine("Instruction number " + instructionCount + " will cause Santa to first enter the basement.");
+
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
