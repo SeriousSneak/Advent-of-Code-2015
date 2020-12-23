1ed8a52b41330f5b45e89c886c37dae2207ff8f2
Andrew Stobart <astobart@microsoft.com>
2020-12-23 13:43:04 -0600

    Day 2 part 2 complete

--- Day 2/Part 2/Program.cs
+++ Day 2/Part 2/Program.cs
@@ -1,9 +1,9 @@
 ﻿/* Advent of Code 2015
  * 
  * Programmer: Andrew Stobart
- * Date: December 22,2020 
+ * Date: December 23,2020 
  * 
- * Day 2 Part 1
+ * Day 2 Part 2
  *
  */
 
@@ -13,12 +13,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-using System.Security.Cryptography.X509Certificates;
 
-namespace Part_1
+namespace Part_2
 {
     class Program
     {
+
         struct dimStruct
         {
             public int length;
@@ -47,20 +47,22 @@ namespace Part_1
             }
 
             int totalPaperNeeded = 0;
+            int totalRibbonNeeded = 0;
 
             for (int x = 0; x < presentsList.Count; x++)
             {
                 dimStruct currentPresent = presentsList[x];
                 totalPaperNeeded += CalcSurfaceArea(currentPresent);
+                totalRibbonNeeded += CalcRibbonLength(currentPresent);
             }
 
             Console.WriteLine("The total amount of paper needed is " + totalPaperNeeded + ".");
+            Console.WriteLine("The total amount of ribbon needed is " + totalRibbonNeeded + ".");
 
 
             Console.WriteLine("");
             Console.WriteLine("Press any key to continue.");
             Console.ReadKey();
-
         }
 
         static int CalcSurfaceArea(dimStruct dimensions)
@@ -68,15 +70,36 @@ namespace Part_1
             int length = dimensions.length;
             int width = dimensions.width;
             int height = dimensions.height;
-            
+
             //calculate area
             int needed = ((2 * length * width) + (2 * width * height) + (2 * height * length));
 
             //add the area of the smallest side
             needed += Math.Min(Math.Min((length * width), (width * height)), (height * length));
-            
+
             return needed;
         }
 
+        static int CalcRibbonLength(dimStruct dimensions)
+        {
+            int length = dimensions.length;
+            int width = dimensions.width;
+            int height = dimensions.height;
+
+            var val = new int[] { length, width, height };
+
+            /*
+             * The following is from https://www.tutorialspoint.com/Chash-program-to-find-Largest-Smallest-Second-Largest-Second-Smallest-in-a-List
+            Largest number: val.Max(z => z);
+            Smallest number: val.Min(z => z);
+            Second largest number: val.OrderByDescending(z => z).Skip(1).First();
+            Second smallest number: val.OrderBy(z => z).Skip(1).First();
+            */
+
+            int needed = (2 * val.Min(z => z)) + (2 * val.OrderBy(z => z).Skip(1).First());  //ribbon for present
+            needed += (length * width * height);  //ribbon for bow
+
+            return needed;
+        }
     }
 }
