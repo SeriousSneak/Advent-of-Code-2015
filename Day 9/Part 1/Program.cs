f2019146831c9ff83f03222ec762077d7c4cacab
Andrew Stobart <astobart@microsoft.com>
2021-01-15 09:42:08 -0600

    Day 9 Part 1 work

---
+++ Day 9/Part 1/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Advent of Code 2015
+ * 
+ * Programmer: Andrew Stobart
+ * Date: 
+ * 
+ * Day 9 Part 1
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
