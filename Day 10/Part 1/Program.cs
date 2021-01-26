1e816225aac908040b103b3fc1f4fed4e4fa5912
Andrew Stobart <astobart@microsoft.com>
2021-01-26 08:31:55 -0600

    Day 10 work

---
+++ Day 10/Part 1/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Advent of Code 2015
+ * 
+ * Programmer: Andrew Stobart
+ * Date:
+ * 
+ * Day 10 Part 1
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
