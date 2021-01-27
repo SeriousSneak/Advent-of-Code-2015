1d21078892596470221e077ab1a4f142f93d3d71
Andrew Stobart <astobart@microsoft.com>
2021-01-27 15:42:37 -0600

    Day 10 work

--- Day 10/Part 1/Program.cs
+++ Day 10/Part 1/Program.cs
@@ -10,6 +10,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,6 +21,27 @@ namespace Part_1
     {
         static void Main(string[] args)
         {
+            //string input = "1321131112";
+            string input = "1";
+
+            //int loopNumber = 5;
+            int loopNumber = 40;
+
+
+            for(int x = 0; x<loopNumber; x++)
+            {
+                bool atEnd = false;
+
+                while (atEnd == false)
+                {
+                    //go through the string
+
+
+
+
+                    //new string is found
+                }
+            }
         }
     }
 }
