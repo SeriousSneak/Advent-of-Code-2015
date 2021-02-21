41de3ec41cd538a335631e696ee0b02df54b9f49
Andrew Stobart <astobart@microsoft.com>
2021-02-20 20:58:06 -0600

    Ground work for Day 11 Part 1

---
+++ Day 11/Part 1/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Advent of Code 2015
+ * 
+ * Programmer: Andrew Stobart
+ * Date:
+ * 
+ * Day 11 Part 1
+ *
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime start = DateTime.Now.ToUniversalTime();
+
+
+
+
+
+            Console.WriteLine("Start: " + start + " UTC.");
+            Console.WriteLine("Finish: " + DateTime.Now.ToUniversalTime() + " UTC.");
+
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
